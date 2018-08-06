263746bece84260b9e278a0d7ac5c7a80a3528e0
Zachary Scott <mail@zzak.io>
2018-08-06 20:36:36 +0900

    Appease the spellcheck linter :raised_hands:

--- .circleci/generate-docs.sh
+++ .circleci/generate-docs.sh
@@ -10,7 +10,7 @@ mkdir -p out
 
 # pandoc markdown to html into out dir
 for f in docs/*.md;  do
-    pandoc "$f" -s -o "out/$(basename ${f%.md}.html)";
+    pandoc "$f" -s -o "out/$(basename "${f%.md}".html)";
 done
 
 # move index and update links
