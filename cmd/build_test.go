eb016c8b6e0c28e968c289c10aa1aa9b4083416e
Hannah Henderson <hannah@circleci.com>
2018-08-27 14:52:18 -0700

    [CIRCLE-12608] Add validateConfigVersion to newLocalExecute command (#76)
    Notify users that version 2.1+ are not available to build locally
    when they try to run a build with a config other than 2.0 or 2
    
    This PR uses a flagset to independently add config value
    
    Flag parsing is disabled in the newLocalExecuteCommand
    in order to pass 'args' through. This commit works around
    that disabled flag paring by adding [config] 'filename'
    to the list of flags separately, making it possible
    to access from the validateConfigVersion function

--- cmd/build_test.go
+++ cmd/build_test.go
@@ -40,4 +40,34 @@ var _ = Describe("build", func() {
 		})
 
 	})
+
+	Describe("config version is tested", func() {
+		It("passes for valid versions", func() {
+			err := validateConfigVersion([]string{"--config", "testdata/config-versions/version-2.yml"})
+			Expect(err).ToNot(HaveOccurred())
+
+			err = validateConfigVersion([]string{"--config", "testdata/config-versions/version-2-0.yml"})
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("passes when other flags are used, too", func() {
+			err := validateConfigVersion([]string{"--config", "testdata/config-versions/version-2.yml", "--job", "foobar"})
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("fails when version number is not '2' or '2.0'", func() {
+			err := validateConfigVersion([]string{"--config", "testdata/config-versions/version-2-1.yml"})
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("fails when version number is not specified", func() {
+			err := validateConfigVersion([]string{"--config", "testdata/config-versions/version-empty.yml"})
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("fails when version is not defined", func() {
+			err := validateConfigVersion([]string{"--config", "testdata/config-versions/version-none.yml"})
+			Expect(err).To(HaveOccurred())
+		})
+	})
 })
