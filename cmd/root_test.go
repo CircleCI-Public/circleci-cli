33b058bee501b3338caec85662027c04b128cb9a
Eric Hu <eric@circleci.com>
2018-09-05 18:01:24 +0700

    Add tests for printing CLI help on flag/arg parsing

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -19,6 +19,29 @@ var _ = Describe("Root", func() {
 			Expect(len(commands.Commands())).To(Equal(14))
 		})
 
+		Context("when run with insufficient arguments", func() {
+			It("returns exit code 255 and prints the subcommand's help body", func() {
+				command := exec.Command(pathCLI, "orb", "publish", "dev")
+				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+				Expect(err).ShouldNot(HaveOccurred())
+				Eventually(session.Out).Should(gbytes.Say("Usage:"))
+				Eventually(session.Out).Should(gbytes.Say("\\s*circleci orb publish dev PATH NAMESPACE ORB LABEL \\[flags\\]"))
+				Eventually(session.Err).Should(gbytes.Say("Error: accepts 4 arg\\(s\\), received 0"))
+				Eventually(session).Should(gexec.Exit(255))
+			})
+		})
+
+		Context("when a subcommand is run with invalid flags", func() {
+			It("returns exit code 255 and prints the subcommand's help body", func() {
+				command := exec.Command(pathCLI, "orb", "publish", "dev", "--foo")
+				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+				Expect(err).ShouldNot(HaveOccurred())
+				Eventually(session.Out).Should(gbytes.Say("Usage:"))
+				Eventually(session.Out).Should(gbytes.Say("\\s*circleci orb publish dev PATH NAMESPACE ORB LABEL \\[flags\\]"))
+				Eventually(session.Err).Should(gbytes.Say("unknown flag: --foo"))
+				Eventually(session).Should(gexec.Exit(255))
+			})
+		})
 	})
 
 	Describe("when run with --help", func() {
