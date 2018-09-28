efcc3d3a4414a4b2ae0349fbeedf92feab05ad2f
Zachary Scott <mail@zzak.io>
2018-09-28 18:45:31 +0900

    :nail_care: appease the linter :raised_hands:

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/CircleCI-Public/circleci-cli/logger"
@@ -12,12 +13,14 @@ import (
 	"github.com/pkg/errors"
 )
 
+// A Client is an HTTP client for our GraphQL endpoint.
 type Client struct {
 	endpoint   string
 	httpClient *http.Client
 	logger     *logger.Logger
 }
 
+// RequestBody is used for serializing a request made up of a query and key/value pair of variables to send to the GraphQL server.
 type RequestBody struct {
 	Query     string                 `json:"query"`
 	Variables map[string]interface{} `json:"variables"`
@@ -41,29 +44,23 @@ func NewAuthorizedRequest(token, query string) *Request {
 	return req
 }
 
-func (c *Client) Run(ctx context.Context, req *Request, resp interface{}) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	default:
-	}
-
+func (c *Client) prepareRequest(ctx context.Context, req *Request, resp interface{}) (*http.Request, *ResponseBody, error) {
 	var requestBody bytes.Buffer
 	requestBodyObj := RequestBody{
 		Query:     req.q,
 		Variables: req.vars,
 	}
 	if err := json.NewEncoder(&requestBody).Encode(requestBodyObj); err != nil {
-		return errors.Wrap(err, "encode body")
+		return nil, nil, errors.Wrap(err, "encode body")
 	}
 	c.logger.Debug(">> variables: %v", req.vars)
 	c.logger.Debug(">> query: %s", req.q)
-	gr := &ResponseBody{
+	body := &ResponseBody{
 		Data: resp,
 	}
 	r, err := http.NewRequest(http.MethodPost, c.endpoint, &requestBody)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 	r.Header.Set("Content-Type", "application/json; charset=utf-8")
 	r.Header.Set("Accept", "application/json; charset=utf-8")
@@ -74,33 +71,57 @@ func (c *Client) Run(ctx context.Context, req *Request, resp interface{}) error 
 	}
 
 	r = r.WithContext(ctx)
+	return r, body, nil
+}
+
+// Run sends an HTTP request to the GraphQL server and deserializes the response or returns an error.
+func (c *Client) Run(ctx context.Context, req *Request, resp interface{}) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+
+	r, body, err := c.prepareRequest(ctx, req, resp)
+	if err != nil {
+		return err
+	}
+
 	res, err := c.httpClient.Do(r)
 	if err != nil {
 		return err
 	}
-	defer res.Body.Close()
+	defer func() {
+		err := res.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 	var buf bytes.Buffer
 	if _, err := io.Copy(&buf, res.Body); err != nil {
 		return errors.Wrap(err, "reading body")
 	}
 	c.logger.Debug("<< %s", buf.String())
-	if err := json.NewDecoder(&buf).Decode(&gr); err != nil {
+	if err := json.NewDecoder(&buf).Decode(&body); err != nil {
 		return errors.Wrap(err, "decoding response")
 	}
-	if len(gr.Errors) > 0 {
-		return gr.bundleErrors()
+	if len(body.Errors) > 0 {
+		return body.bundleErrors()
 	}
 	return nil
 }
 
+// ResponseError wraps the error type returned from the GraphQL server.
 type ResponseError struct {
 	Message string
 }
 
+// Error returns the messages of a response error.
 func (e ResponseError) Error() string {
 	return "graphql: " + e.Message
 }
 
+// ResponseBody maps the data returned from the GraphQL server.
 type ResponseBody struct {
 	Data   interface{}
 	Errors []ResponseError
