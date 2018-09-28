0f6c2ef9ffb973e1f26b07a7786a51097f617707
Zachary Scott <mail@zzak.io>
2018-09-28 18:04:32 +0900

    Import graphql json tests and convert using plain testing package

---
+++ client/client_test.go
@@ -0,0 +1,192 @@
+package client
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/CircleCI-Public/circleci-cli/logger"
+)
+
+func TestDoJSON(t *testing.T) {
+	var calls int
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		calls++
+
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+
+		if string(b) != `{"query":"query {}","variables":null}`+"\n" {
+			t.Errorf("expected %s", string(b))
+		}
+
+		io.WriteString(w, `{
+			"data": {
+				"something": "yes"
+			}
+		}`)
+	}))
+	defer srv.Close()
+
+	ctx := context.Background()
+	client := NewClient(srv.URL, logger.NewLogger(false))
+
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+	var responseData map[string]interface{}
+	err := client.Run(ctx, &Request{q: "query {}"}, &responseData)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if calls != 1 {
+		t.Errorf("expected %s", string(calls))
+	}
+
+	if responseData["something"] != "yes" {
+		t.Errorf("expected %s", responseData["something"])
+	}
+}
+
+func TestQueryJSON(t *testing.T) {
+	var calls int
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		calls++
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+		if string(b) != `{"query":"query {}","variables":{"username":"matryer"}}`+"\n" {
+			t.Errorf("expected %s", b)
+		}
+		_, err = io.WriteString(w, `{"data":{"value":"some data"}}`)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	}))
+	defer srv.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	client := NewClient(srv.URL, logger.NewLogger(false))
+
+	req := NewRequest("query {}")
+	req.Var("username", "matryer")
+
+	// check variables
+	if req == nil {
+		t.Errorf("expected %s", req)
+	}
+
+	if req.vars["username"] != "matryer" {
+		t.Errorf("expcted %s", req.vars["username"])
+	}
+
+	var resp struct {
+		Value string
+	}
+	err := client.Run(ctx, req, &resp)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if calls != 1 {
+		t.Errorf("expected %s", string(calls))
+	}
+
+	if resp.Value != "some data" {
+		t.Errorf("expected %s", resp.Value)
+	}
+}
+
+func TestDoJSONErr(t *testing.T) {
+	var calls int
+	server := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
+		calls++
+
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+
+		if string(body) != `{"query":"query {}","variables":null}`+"\n" {
+			t.Errorf("expected %s", body)
+		}
+
+		io.WriteString(writer,
+			`{
+				"errors": [
+					{
+						"message": "Something went wrong"
+					},
+					{
+						"message": "Something else went wrong"
+					}
+				]
+			}`)
+	}))
+
+	defer server.Close()
+
+	ctx := context.Background()
+	client := NewClient(server.URL, logger.NewLogger(false))
+
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	var responseData map[string]interface{}
+	err := client.Run(ctx, &Request{q: "query {}"}, &responseData)
+
+	if err == nil {
+		t.Errorf(err.Error())
+	}
+
+	if err.Error() != "Something else went wrong: Something went wrong" {
+		t.Errorf("expected %s", err.Error())
+	}
+}
+
+func TestHeader(t *testing.T) {
+	var calls int
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		calls++
+		if r.Header.Get("X-Custom-Header") != "123" {
+			t.Errorf("expected %s", r.Header.Get("X-Custom-Header"))
+		}
+
+		_, err := io.WriteString(w, `{"data":{"value":"some data"}}`)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	}))
+	defer srv.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	client := NewClient(srv.URL, logger.NewLogger(false))
+
+	req := NewRequest("query {}")
+	req.Header.Set("X-Custom-Header", "123")
+
+	var resp struct {
+		Value string
+	}
+	err := client.Run(ctx, req, &resp)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if calls != 1 {
+		t.Errorf("expected %s", string(calls))
+	}
+
+	if resp.Value != "some data" {
+		t.Errorf("expected %s", resp.Value)
+	}
+}
