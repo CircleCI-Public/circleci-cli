00ee49266ecd4d9f01e1073813ae2f2b6f0b08cc
Zachary Scott <mail@zzak.io>
2018-10-17 22:40:44 +0900

    Wrap response data with an internal struct
    This means we can treat runtime errors the same as out-of-band GraphQL
    errors without exposing those or the wrapped Data field of internals.

--- client/client.go
+++ client/client.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/CircleCI-Public/circleci-cli/logger"
 	"github.com/CircleCI-Public/circleci-cli/version"
@@ -83,6 +84,27 @@ func (request *Request) Encode() (bytes.Buffer, error) {
 	var body bytes.Buffer
 	err := json.NewEncoder(&body).Encode(request)
 	return body, err
+}
+
+type Response struct {
+	Data   interface{}
+	Errors ResponseErrors
+}
+
+type ResponseErrors []ResponseError
+
+type ResponseError struct {
+	Message string
+}
+
+func (errs ResponseErrors) Error() string {
+	messages := []string{}
+
+	for i := range errs {
+		messages = append(messages, errs[i].Message)
+	}
+
+	return strings.Join(messages, "\n")
 }
 
 // getServerAddress returns the full address to the server
@@ -166,8 +188,16 @@ func (cl *Client) Run(ctx context.Context, log *logger.Logger, request *Request,
 		}
 	}()
 
-	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+	wrappedResponse := &Response{
+		Data: resp,
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&wrappedResponse); err != nil {
 		return errors.Wrap(err, "decoding response")
+	}
+
+	if len(wrappedResponse.Errors) > 0 {
+		return wrappedResponse.Errors
 	}
 
 	log.Debug("<< %+v", resp)
