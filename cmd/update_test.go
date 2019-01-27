04f93055e69f46cf9dacf262806575bc975294be
Zachary Scott <mail@zzak.io>
2019-01-27 23:19:30 +0900

    [CIRCLE-15483]: Catch all 400s from GitHub release api in `update`
    We want to make sure we cover all bases and catch every 4** error
    GitHub might return to us and display a verbose message for how to
    treat these kinds of errors.
    
    Since the majority of the the time this error occurs when the user
    hits the API without setting a GITHUB_TOKEN, and results in a rate
    limt for their IP address; these directions should help solve this problem.

--- cmd/update_test.go
+++ cmd/update_test.go
@@ -141,4 +141,46 @@ var _ = Describe("Update", func() {
 			Eventually(session).Should(gexec.Exit(0))
 		})
 	})
+
+	Describe("When Github returns a 403 error", func() {
+		BeforeEach(func() {
+			command = exec.Command(pathCLI,
+				"update", "check",
+				"--github-api", tempSettings.TestServer.URL(),
+			)
+
+			tempSettings.TestServer.Reset()
+			tempSettings.TestServer.AppendHandlers(
+				ghttp.CombineHandlers(
+					ghttp.VerifyRequest("GET", "/repos/CircleCI-Public/circleci-cli/releases"),
+					ghttp.RespondWith(http.StatusForbidden, []byte("Forbidden")),
+				),
+			)
+		})
+
+		It("should print a helpful error message & exit 255", func() {
+			session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).ShouldNot(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(255))
+
+			// TODO: This should exit with error status 1, since 255 is a
+			// special error status for: "exit status outside of range".
+			stderr := session.Wait().Err.Contents()
+			Expect(string(stderr)).To(ContainSubstring(`Error: Failed to query the GitHub API for updates.
+
+This is most likely due to GitHub rate-limiting on unauthenticated requests.
+
+To have the circleci-cli make authenticated requests please:
+
+  1. Generate a token at https://github.com/settings/tokens
+  2. Set the token by either adding it to your ~/.gitconfig
+     setting the GITHUB_TOKEN environment variable.
+
+Instructions for generating a token can be found at:
+https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+
+We call the github releases API to look for new releases.
+More information about that API can be found here: https://developer.github.com/v3/repos/releases/`))
+		})
+	})
 })
