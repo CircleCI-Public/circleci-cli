f027cbf69cb0becd9b714a36ef682d1d8cdcb97c
Stella Lok <oinvertedworld@gmail.com>
2019-09-09 17:37:54 +0800

    - call validateToken - update command description

--- cmd/orb.go
+++ cmd/orb.go
@@ -178,14 +178,17 @@ Example: 'circleci orb publish increment foo/orb.yml foo/bar minor' => foo/bar@1
 
 	unlistCmd := &cobra.Command{
 		Use:   "unlist <namespace>/<orb> <true|false>",
-		Short: "Enables or disables the specified orb for listing in the registry",
-		Long: `Enables or disables the specified orb for listing in the registry.
+		Short: "Enables or disables an orb for listing in the registry",
+		Long: `Enables or disables an orb for listing in the registry.
 
 Example: Run 'circleci orb unlist foo/bar true' to disable the listing of the
 orb in the registry and 'circleci orb unlist foo/bar false' to re-enable the
 listing of the orb in the registry.`,
 		RunE: func(_ *cobra.Command, _ []string) error {
 			return setOrbListStatus(opts)
+		},
+		PreRunE: func(_ *cobra.Command, _ []string) error {
+			return validateToken(opts.cfg)
 		},
 		Args: cobra.ExactArgs(2),
 	}
