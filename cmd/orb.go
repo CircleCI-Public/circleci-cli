a0c153565f9d7133011d61e3e4ed0712399bdbb1
Scott Wheeler <scott_wheeler@outlook.com>
2019-11-15 11:13:35 -0500

    added orb init command

--- cmd/orb.go
+++ cmd/orb.go
@@ -16,6 +16,12 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/spf13/cobra"
+
+	"io"
+	"net/http"
+	"os"
+	"archive/zip"
+	"path/filepath"
 )
 
 type orbOptions struct {
@@ -246,6 +252,16 @@ Please note that at this time all orbs created in the registry are world-readabl
 	}
 	orbCreate.Flags().BoolVar(&opts.noPrompt, "no-prompt", false, "Disable prompt to bypass interactive UI.")
 
+	orbInit := &cobra.Command{
+		Use: "init <orb name>",
+		Short: "Initialize a new orb using the Orb starter kit",
+		Long: `Initialize a new orb using the Orb starter kit`,
+		RunE: func(_ *cobra.Command, _ []string) error {
+			return initOrb(opts)
+		},
+		Args: cobra.ExactArgs(1),
+	}
+
 	orbCommand := &cobra.Command{
 		Use:   "orb",
 		Short: "Operate on orbs",
@@ -269,6 +285,7 @@ Please note that at this time all orbs created in the registry are world-readabl
 	orbCommand.AddCommand(unlistCmd)
 	orbCommand.AddCommand(sourceCommand)
 	orbCommand.AddCommand(orbInfoCmd)
+	orbCommand.AddCommand(orbInit);
 
 	return orbCommand
 }
@@ -649,6 +666,105 @@ func promoteOrb(opts orbOptions) error {
 
 	fmt.Printf("Orb `%s` was promoted to `%s/%s@%s`.\n", ref, namespace, orb, response.HighestVersion)
 	fmt.Println("Please note that this is an open orb and is world-readable.")
+	return nil
+}
+
+func initOrb(opts orbOptions) error {
+	var err error
+	name := opts.args[0]
+	
+	fmt.Printf("Downloading orb-starter-kit\n")
+
+	fileURL := "https://github.com/scwheele/orb-starter-kit/archive/master.zip"
+	
+	resp, err := http.Get(fileURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create("orb-starter-kit.zip")
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	
+	fmt.Printf("Unzipping orb-starter-kit\n")
+	err = Unzip("orb-starter-kit.zip", "orb-starter-kit")
+	if err != nil {
+		return err
+	}
+
+	os.Rename("orb-starter-kit/orb-starter-kit-master", name)
+	fmt.Printf("Cleaning up directory\n")
+
+	os.Remove("orb-starter-kit")
+	os.Remove("orb-starter-kit.zip")
+
+	fmt.Printf("Created Orb called %s\n", name)
+
+	return nil
+}
+
+// From https://stackoverflow.com/questions/20357223/easy-way-to-unzip-file-with-golang
+func Unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	os.MkdirAll(dest, 0755)
+
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(dest, f.Name)
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
