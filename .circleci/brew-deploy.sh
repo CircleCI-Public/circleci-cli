98cbb5a5a1968a7ab93e1a2d287f365e95769672
Marc O'Morain <marc@circleci.com>
2020-04-24 14:39:10 +0100

    Fix interactive terminal issue
    Deployments to homebrew are blocked because the deployment system is
    waiting for interactive input. Set the TERM flag to value that should
    bypass this.

--- .circleci/brew-deploy.sh
+++ .circleci/brew-deploy.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
-
 set -e
 
 # Remove the original circleci-agent from /usr/local/bin for homebrew
 rm /usr/local/bin/circleci
 # Install the latest circleci from homebrew
+brew update
 brew install circleci
 
 VERSION=$("$DESTDIR"/circleci version)
 TAG="v$(ruby -e "puts '$VERSION'.split('+')[0]")"
 REVISION=$(git rev-parse "$(ruby -e "puts '$VERSION'.split('+')[1]")")
 echo "Bumping circleci to $TAG+$REVISION"
-brew bump-formula-pr --strict \
-  --tag="$TAG" \
-  --revision="$REVISION" \
-  circleci
+brew bump-formula-pr --strict --tag="$TAG" --revision="$REVISION" circleci
