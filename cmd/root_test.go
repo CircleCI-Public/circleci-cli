f5eed321276df2cb2c2da6ad7a4c5ff261cbc78f
Marc O'Morain <marc@circleci.com>
2020-05-19 10:23:30 +0100

    Move a couple more tests to cucumber

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -19,49 +19,6 @@ var _ = Describe("Root", func() {
 		})
 	})
 
-	Describe("Help text", func() {
-		It("shows a link to the docs", func() {
-			command := exec.Command(pathCLI, "--help")
-			session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-			Expect(err).ShouldNot(HaveOccurred())
-
-			Eventually(session.Out).Should(gbytes.Say(`Use CircleCI from the command line.
-
-This project is the seed for CircleCI's new command-line application.
-
-For more help, see the documentation here: https://circleci.com/docs/2.0/local-cli/
-`))
-			Eventually(session).Should(gexec.Exit(0))
-		})
-
-		Context("if user changes host settings through configuration", func() {
-			var (
-				tempSettings *clitest.TempSettings
-				command      *exec.Cmd
-			)
-
-			BeforeEach(func() {
-				tempSettings = clitest.WithTempSettings()
-
-				command = commandWithHome(pathCLI, tempSettings.Home, "--help")
-
-				tempSettings.Config.Write([]byte(`host: foo.bar`))
-			})
-
-			AfterEach(func() {
-				tempSettings.Cleanup()
-			})
-
-			It("doesn't link to docs if user changes --host", func() {
-				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-				Expect(err).ShouldNot(HaveOccurred())
-
-				Consistently(session.Out).ShouldNot(gbytes.Say("For more help, see the documentation here: https://circleci.com/docs/2.0/local-cli/"))
-				Eventually(session).Should(gexec.Exit(0))
-			})
-		})
-	})
-
 	Describe("build without auto update", func() {
 		var (
 			command      *exec.Cmd
@@ -137,39 +94,6 @@ For more help, see the documentation here: https://circleci.com/docs/2.0/local-c
 			Eventually(session.Out).Should(gbytes.Say("update      Update the tool to the latest version"))
 
 			Eventually(session).Should(gexec.Exit(0))
-		})
-	})
-
-	Describe("token in help text", func() {
-		var (
-			command      *exec.Cmd
-			tempSettings *clitest.TempSettings
-		)
-
-		BeforeEach(func() {
-			tempSettings = clitest.WithTempSettings()
-
-			command = commandWithHome(pathCLI, tempSettings.Home,
-				"help", "--skip-update-check",
-			)
-		})
-
-		AfterEach(func() {
-			tempSettings.Cleanup()
-		})
-
-		Describe("existing config file", func() {
-			BeforeEach(func() {
-				tempSettings.Config.Write([]byte(`token: secret`))
-			})
-
-			It("does not include the users token in help text", func() {
-				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-				Expect(err).ShouldNot(HaveOccurred())
-				Eventually(session.Err.Contents()).Should(BeEmpty())
-
-				Ω(session.Wait().Out.Contents()).ShouldNot(ContainSubstring("your token for using CircleCI (default \"secret\")"))
-			})
 		})
 	})
 })
