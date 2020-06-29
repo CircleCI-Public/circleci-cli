ea2779b3481656a3c3cd6f1ee52552279dfd61f1
Alex Engelberg <alex.engelberg@circleci.com>
2020-06-29 12:04:36 -0700

    Fix brew release script

--- .circleci/brew-deploy.sh
+++ .circleci/brew-deploy.sh
@@ -8,7 +8,7 @@ brew update
 brew install circleci
 
 VERSION=$("$DESTDIR"/circleci version)
-TAG="v$(ruby -e "puts '$VERSION'.split('+')[0]")"
-REVISION=$(git rev-parse "$(ruby -e "puts '$VERSION'.split('+')[1]")")
+TAG="v$(ruby -e "puts '$VERSION'.split(/[ +]/)[0]")"
+REVISION=$(git rev-parse "$(ruby -e "puts '$VERSION'.split(/[ +]/)[1]")")
 echo "Bumping circleci to $TAG+$REVISION"
 brew bump-formula-pr --strict --tag="$TAG" --revision="$REVISION" circleci
