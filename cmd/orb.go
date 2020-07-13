98181dfc5d6e9940e6a6171bca961770614c6303
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-07-13 12:57:20 +0100

    Hide orb pack command for beta.

--- cmd/orb.go
+++ cmd/orb.go
@@ -254,7 +254,8 @@ Please note that at this time all orbs created in the registry are world-readabl
 		RunE: func(_ *cobra.Command, _ []string) error {
 			return packOrb(opts)
 		},
-		Args: cobra.ExactArgs(1),
+		Args:   cobra.ExactArgs(1),
+		Hidden: true,
 	}
 
 	orbCreate.Flags().BoolVar(&opts.integrationTesting, "integration-testing", false, "Enable test mode to bypass interactive UI.")
