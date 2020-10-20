6645d4389878eb2ecc6b4a4645a366259cd78f2f
John Swanson <j@agh.io>
2020-10-20 13:36:25 -0700

    Revert "`orb init` improvements (#496)"
    This reverts commit faf8544f091df216acce80e8de6ca423c931a2aa.

--- process/process.go
+++ process/process.go
@@ -12,25 +12,15 @@ import (
 // of "file", escaping instances of "<<" within the file before returning, when
 // the <<include()>> parameter is the string passed.
 func MaybeIncludeFile(s string, orbDirectory string) (string, error) {
-	// View: https://regexr.com/599mq
-	includeRegex := regexp.MustCompile(`<<[\s]*include\(([\w\/\.]+)\)?[\s]*>>`)
-
-	// only find up to 2 matches, because we throw an error if we find >1
-	includeMatches := includeRegex.FindAllStringSubmatch(s, 2)
-	if len(includeMatches) > 1 {
-		return "", fmt.Errorf("multiple include statements: '%s'", s)
+	// View: https://regexr.com/582gb
+	includeRegex, err := regexp.Compile(`(?U)^<<\s*include\((.*\/*[^\/]+)\)\s*?>>$`)
+	if err != nil {
+		return s, err
 	}
 
-	if len(includeMatches) == 1 {
-		match := includeMatches[0]
-		fullMatch, subMatch := match[0], match[1]
-
-		// throw an error if the entire string wasn't matched
-		if fullMatch != s {
-			return "", fmt.Errorf("entire string must be include statement: '%s'", s)
-		}
-
-		filepath := filepath.Join(orbDirectory, subMatch)
+	includeMatches := includeRegex.FindStringSubmatch(s)
+	if len(includeMatches) > 0 {
+		filepath := filepath.Join(orbDirectory, includeMatches[1])
 		file, err := ioutil.ReadFile(filepath)
 		if err != nil {
 			return "", fmt.Errorf("could not open %s for inclusion", filepath)
