178bec90837370b665f0d5e515fc0ea099d05afd
Alex Engelberg <alex.engelberg@circleci.com>
2021-02-16 11:27:58 -0800

    Add tests for CLI validation

--- cli-integration-tests/src/circleci/cli/test/util.clj
+++ cli-integration-tests/src/circleci/cli/test/util.clj
@@ -1,5 +1,7 @@
 (ns circleci.cli.test.util
   (:require [clojure.java.io :as io]
+            [clojure.java.shell :refer [sh]]
+            [clojure.string :as str]
             [clojure.test :refer :all]
             [greenlight.step :as step :refer [defstep]]))
 
@@ -20,3 +22,39 @@
     (io/delete-file filename)
     (catch Exception e
       (printf "WARN: couldn't delete %s: %s\n" filename e))))
+
+(defn shell!
+  "Reusable step that runs a shell command and validates its output."
+  [args
+   {:keys [exit exit-nonzero out-contains err-contains]}]
+  #::step
+  {:title (str/join " " args)
+   :name 'shell!
+   :test (fn [_]
+           (let [result (apply sh args)
+                 out (String. (:out result))
+                 err (:err result)
+                 exited-as-expected?
+                 (if exit-nonzero
+                   (is (not= 0 (:exit result)))
+                   (is (= (or exit 0) (:exit result))))]
+             (when-not exited-as-expected?
+               (println "EXITED WITH" (:exit result))
+               (println "STDOUT:")
+               (println out)
+               (println "--------")
+               (println "STDERR:")
+               (println err)
+               (println "--------"))
+             (when out-contains
+               (is (str/includes? out out-contains)))
+             (when err-contains
+               (is (str/includes? err err-contains)))))})
+
+(defn cli!
+  "Reusable step that runs a CLI command and validates its output."
+  [args verify-opts]
+  ;; Add --skip-update-check because we don't need it in CI
+  ;; and the update spinner adds a bunch of spam to the output
+  (shell! (into ["circleci" "--skip-update-check"] args)
+          verify-opts))
--- cli-integration-tests/src/circleci/cli/test/validate.clj
+++ cli-integration-tests/src/circleci/cli/test/validate.clj
@@ -4,9 +4,24 @@
             [greenlight.step :as step :refer [defstep]]))
 
 (deftest validate-good-config
-  "Validate config that is valid"
+  "Validating good config says it is valid"
   (util/add-file! "config.yml" "
+version: 2.1
 jobs:
   build:
     machine: true
-    steps: [checkout]"))
+    steps: [checkout]")
+  (util/cli! ["config" "validate" "-c" "config.yml"]
+             {:out-contains "Config file at config.yml is valid."}))
+
+
+(deftest validate-bad-config
+  "Validating bad config says it is not valid"
+  (util/add-file! "config.yml" "
+version: 2.1
+jobs:
+  build:
+    steps: [checkout]")
+  (util/cli! ["config" "validate" "-c" "config.yml"]
+             {:exit-nonzero 1
+              :err-contains "A job must have one of `docker`, `machine`, `macos` or `executor`"}))
