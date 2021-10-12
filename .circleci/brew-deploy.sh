6d37188dcad6033cb8a3c85e3db9ca71b0f788e7
Kelvin Li <kelvin.li@circleci.com>
2021-10-12 11:36:52 -0400

    Sample error checking

--- .circleci/brew-deploy.sh
+++ .circleci/brew-deploy.sh
@@ -10,4 +10,18 @@ VERSION=$("$DESTDIR"/circleci version)
 TAG="v$(ruby -e "puts '$VERSION'.split(/[ +]/)[0]")"
 REVISION=$(git rev-parse "$(ruby -e "puts '$VERSION'.split(/[ +]/)[1]")")
 echo "Bumping circleci to $TAG+$REVISION"
-brew bump-formula-pr --strict --tag="$TAG" --revision="$REVISION" circleci
+
+
+brew-bump="brew bump-formula-pr --strict --tag=$TAG --revision=$REVISION circleci"
+
+if result=$(eval brew-bump 2>&1); then
+    echo $result
+else
+    exitcode=$?
+    if [[ "$result" == *"Duplicate PRs should not be opened."* ]]; then
+        exit 0
+    else 
+        echo $result
+        exit $exitcode
+    fi
+fi
