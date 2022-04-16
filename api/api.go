8d1d307cfc7f62026371f54a32ca79134d5f8233
mkusaka <hinoshita1992@gmail.com>
2022-04-17 00:39:15 +0900

    apply go fmt

--- api/api.go
+++ api/api.go
@@ -1878,7 +1878,7 @@ func ListOrbCategories(cl *graphql.Client) (*OrbCategoriesForListing, error) {
 var errorMessage = `Unable to follow project`
 
 func FollowProject(config settings.Config, vcs string, owner string, projectName string) (FollowedProject, error) {
-	
+
 	requestPath := fmt.Sprintf("%s/api/v1.1/project/%s/%s/%s/follow", config.Host, vcs, owner, projectName)
 	r, err := http.NewRequest(http.MethodPost, requestPath, nil)
 	if err != nil {
--- api/context.go
+++ api/context.go
@@ -7,16 +7,16 @@ import (
 // An EnvironmentVariable has a Variable, a ContextID (its owner), and a
 // CreatedAt date.
 type EnvironmentVariable struct {
-	Variable string
+	Variable  string
 	ContextID string
 	CreatedAt time.Time
 }
 
 // A Context is the owner of EnvironmentVariables.
-type Context struct{
+type Context struct {
 	CreatedAt time.Time `json:"created_at"`
-	ID string `json:"id"`
-	Name string `json:"name"`
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
 }
 
 // ContextInterface is the interface to interact with contexts and environment
@@ -25,7 +25,7 @@ type ContextInterface interface {
 	Contexts(vcs, org string) (*[]Context, error)
 	ContextByName(vcs, org, name string) (*Context, error)
 	DeleteContext(contextID string) error
-	CreateContext(vcs, org, name string) (error)
+	CreateContext(vcs, org, name string) error
 
 	EnvironmentVariables(contextID string) (*[]EnvironmentVariable, error)
 	CreateEnvironmentVariable(contextID, variable, value string) error
