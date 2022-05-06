7f2024f8d233c6bf39f0d24f1b8f05a2a7c0200c
corinnesollows <corinne.sollows@gmail.com>
2022-05-06 10:44:00 -0300

    Tests created

--- cmd/context_test.go
+++ cmd/context_test.go
@@ -11,32 +11,88 @@ import (
 )
 
 var _ = Describe("Context integration tests", func() {
-	Describe("when listing contexts without a token", func() {
-		var (
-			command      *exec.Cmd
-			tempSettings *clitest.TempSettings
-		)
+	var (
+		tempSettings *clitest.TempSettings
+		token        string = "testtoken"
+		command      *exec.Cmd
+	)
 
-		BeforeEach(func() {
-			tempSettings = clitest.WithTempSettings()
-			command = commandWithHome(pathCLI, tempSettings.Home,
-				"context", "list", "github", "foo",
-				"--skip-update-check",
-				"--token", "",
-			)
-		})
-
-		It("instructs the user to run 'circleci setup' and create a new token", func() {
-			By("running the command")
-			session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-
-			Expect(err).ShouldNot(HaveOccurred())
-			Eventually(session.Err).Should(gbytes.Say(`Error: please set a token with 'circleci setup'
-You can create a new personal API token here:
-https://circleci.com/account/api`))
-			Eventually(session).Should(clitest.ShouldFail())
-		})
+	BeforeEach(func() {
+		tempSettings = clitest.WithTempSettings()
 	})
 
-	// TODO: add integration tests for happy path cases
+	AfterEach(func() {
+		tempSettings.Close()
+	})
+
+	Context("create, with interactive prompts", func() {
+
+		Describe("when listing contexts without a token", func() {
+			BeforeEach(func() {
+				command = commandWithHome(pathCLI, tempSettings.Home,
+					"context", "list", "github", "foo",
+					"--skip-update-check",
+					"--token", "",
+				)
+			})
+
+			It("instructs the user to run 'circleci setup' and create a new token", func() {
+				By("running the command")
+				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+
+				Expect(err).ShouldNot(HaveOccurred())
+				Eventually(session.Err).Should(gbytes.Say(`Error: please set a token with 'circleci setup'
+You can create a new personal API token here:
+https://circleci.com/account/api`))
+				Eventually(session).Should(clitest.ShouldFail())
+			})
+		})
+
+		Describe("using an org id to create a context", func() {
+			BeforeEach(func() {
+				command = exec.Command(pathCLI,
+					"context", "create",
+					"--skip-update-check",
+					"--token", token,
+					"--host", tempSettings.TestServer.URL(),
+					"--integration-testing",
+					"foo-context",
+					"--org-id", `"bb604b45-b6b0-4b81-ad80-796f15eddf87"`,
+				)
+			})
+
+			It("user creating new context", func() {
+				By("running the command")
+				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+
+				Expect(err).ShouldNot(HaveOccurred())
+				Eventually(session.Err).Should(gbytes.Say(`Error: please use either an orgid or vcs and org name to create context`))
+				Eventually(session).Should(clitest.ShouldFail())
+			})
+		})
+
+		Describe("using an vcs and org name to create a context", func() {
+			BeforeEach(func() {
+				command = exec.Command(pathCLI,
+					"context", "create",
+					"--skip-update-check",
+					"--token", token,
+					"--host", tempSettings.TestServer.URL(),
+					"--integration-testing",
+					"BITBUCKET",
+					"test-org",
+					"foo-context",
+				)
+			})
+
+			It("user creating new context", func() {
+				By("running the command")
+				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+
+				Expect(err).ShouldNot(HaveOccurred())
+				Eventually(session.Err).Should(gbytes.Say(`Error: please use either an orgid or vcs and org name to create context`))
+				Eventually(session).Should(clitest.ShouldFail())
+			})
+		})
+	})
 })
