3e6886d50b4d0e6770fe7a8c6612652d41a9d92f
davidmdm <desmarais.michaud.david@gmail.com>
2022-06-03 11:32:14 -0400

    added godoc comments policy command and api

--- cmd/policy/policy.go
+++ cmd/policy/policy.go
@@ -13,8 +13,10 @@ import (
 	"github.com/CircleCI-Public/circleci-cli/settings"
 )
 
+// validator is a cobra command and args validator to be run as persisten PreRun for every policy command.
 type validator func(cmd *cobra.Command, args []string) error
 
+// NewCommand creates the root policy command with all policy subcommands attached.
 func NewCommand(config *settings.Config, preRunE validator) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:               "policy",
