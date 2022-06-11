da0e4ed5a90f55d4c1acb5fe46021da9e9213ee5
Conor Mongey <conor@mongey.net>
2022-06-11 21:48:56 +0100

    Add support for multiple <<includes(file)>> and embedded <<includes(file)>>

--- process/process.go
+++ process/process.go
@@ -15,29 +15,20 @@ func MaybeIncludeFile(s string, orbDirectory string) (string, error) {
 	// View: https://regexr.com/599mq
 	includeRegex := regexp.MustCompile(`<<[\s]*include\(([-\w\/\.]+)\)?[\s]*>>`)
 
-	// only find up to 2 matches, because we throw an error if we find >1
-	includeMatches := includeRegex.FindAllStringSubmatch(s, 2)
-	if len(includeMatches) > 1 {
-		return "", fmt.Errorf("multiple include statements: '%s'", s)
-	}
+	includeMatches := includeRegex.FindAllStringSubmatch(s, -1)
 
-	if len(includeMatches) == 1 {
-		match := includeMatches[0]
+	for _, match := range includeMatches {
 		fullMatch, subMatch := match[0], match[1]
-
-		// throw an error if the entire string wasn't matched
-		if fullMatch != s {
-			return "", fmt.Errorf("entire string must be include statement: '%s'", s)
-		}
 
 		filepath := filepath.Join(orbDirectory, subMatch)
 		file, err := ioutil.ReadFile(filepath)
+
 		if err != nil {
 			return "", fmt.Errorf("could not open %s for inclusion", filepath)
 		}
-		escaped := strings.ReplaceAll(string(file), "<<", "\\<<")
 
-		return escaped, nil
+		escaped := strings.ReplaceAll(string(file), "<<", "\\<<")
+		s = strings.ReplaceAll(s, fullMatch, escaped)
 	}
 
 	return s, nil
