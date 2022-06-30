cdbf78eb3b51738155f339c5d7201cf679ef5ec3
corinnesollows <corinne.sollows@gmail.com>
2022-06-30 10:45:50 -0300

    Warning Error

--- cmd/open.go
+++ cmd/open.go
@@ -13,9 +13,8 @@ import (
 
 // errorMessage string containing the error message displayed in both the open command and the follow command
 var errorMessage = `
-Unable detect which URL should be opened. This command is intended to be run from
-a git repository with a remote named 'origin' that is hosted on Github or Bitbucket
-Error`
+This command is intended to be run from a git repository with a remote named 'origin' that is hosted on Github or Bitbucket only. 
+We are not currently supporting any other hosts.`
 
 //projectUrl uses the provided values to create the url to open
 func projectUrl(remote *git.Remote) string {
@@ -31,8 +30,12 @@ func openProjectInBrowser() error {
 	if err != nil {
 		return errors.Wrap(err, errorMessage)
 	}
-
-	return browser.OpenURL(projectUrl(remote))
+	//check that project url contains github or bitbucket; our legacy vcs
+	if remote.VcsType == git.GitHub || remote.VcsType == git.Bitbucket {
+		return browser.OpenURL(projectUrl(remote))
+	}
+	//if not warn user their vcs is not supported
+	return errors.New(errorMessage)
 }
 
 // newOpenCommand creates the cli command open
