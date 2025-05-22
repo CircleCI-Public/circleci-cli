b429b96628a1ccfb7325cf9015ed598004059585
Jacqueline Garcia <jackieg.017@gmail.com>
2025-05-22 08:59:56 -0700

    clean up tests, and test checkoutRef and configRef makes a valid request

--- api/trigger/trigger_rest_test.go
+++ api/trigger/trigger_rest_test.go
@@ -1,7 +1,9 @@
 package trigger_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -25,47 +27,104 @@ func getTriggerRestClient(server *httptest.Server) (trigger.TriggerClient, error
 }
 
 func Test_triggerRestClient_CreateTrigger(t *testing.T) {
-	const (
-		vcsType              = "circleci"
-		orgName              = "test-org"
-		projectID            = "test-project-id"
-		pipelineDefinitionID = "test-pipeline-definition-id"
-		repoID               = "test-repo-id"
-		testName             = "test-trigger"
-		description          = "test-description"
-		eventPreset          = "all-pushes"
-		configRef            = ""
-		checkoutRef          = ""
-	)
-
 	tests := []struct {
 		name    string
+		options trigger.CreateTriggerOptions
 		handler http.HandlerFunc
 		want    *trigger.CreateTriggerInfo
 		wantErr bool
 	}{
 		{
 			name: "Should handle a successful request with CreateTrigger",
+			options: trigger.CreateTriggerOptions{
+				ProjectID:            "testProjectID",
+				PipelineDefinitionID: "pipelineDefinitionID",
+				Name:                 "testName",
+				Description:          "description",
+				RepoID:               "repoID",
+				EventPreset:          "eventPreset",
+			},
 			handler: func(w http.ResponseWriter, r *http.Request) {
 				assert.Equal(t, r.Header.Get("circle-token"), "token")
 				assert.Equal(t, r.Header.Get("accept"), "application/json")
 				assert.Equal(t, r.Header.Get("user-agent"), version.UserAgent())
 
 				assert.Equal(t, r.Method, "POST")
-				assert.Equal(t, r.URL.Path, fmt.Sprintf("/api/v2/projects/%s/pipeline-definitions/%s/triggers", projectID, pipelineDefinitionID))
+				assert.Equal(t, r.URL.Path, fmt.Sprintf("/api/v2/projects/%s/pipeline-definitions/%s/triggers", "testProjectID", "pipelineDefinitionID"))
 
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusOK)
-				_, err := w.Write([]byte(`
-				{
-					"id": "123",
-					"name": "test-trigger"
-				}`))
+
+				resp, err := json.Marshal(map[string]any{
+					"id":   "123",
+					"name": "testName",
+				})
+				assert.NilError(t, err)
+				_, err = w.Write(resp)
 				assert.NilError(t, err)
 			},
 			want: &trigger.CreateTriggerInfo{
 				Id:   "123",
-				Name: testName,
+				Name: "testName",
+			},
+		},
+		{
+			name: "Should handle a successful request with CreateTrigger with checkoutRef and configRef",
+			options: trigger.CreateTriggerOptions{
+				ProjectID:            "projectID",
+				PipelineDefinitionID: "pipelineDefinitionID",
+				Name:                 "testName",
+				Description:          "description",
+				RepoID:               "repoID",
+				EventPreset:          "eventPreset",
+				ConfigRef:            "configRef",
+				CheckoutRef:          "checkoutRef",
+			},
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, r.Header.Get("circle-token"), "token")
+				assert.Equal(t, r.Header.Get("accept"), "application/json")
+				assert.Equal(t, r.Header.Get("user-agent"), version.UserAgent())
+
+				assert.Equal(t, r.Method, "POST")
+				assert.Equal(t, r.URL.Path, fmt.Sprintf("/api/v2/projects/%s/pipeline-definitions/%s/triggers", "projectID", "pipelineDefinitionID"))
+
+				expectedRequestBody := map[string]any{
+					"name":         "testName",
+					"description":  "description",
+					"event_preset": "eventPreset",
+					"event_source": map[string]any{
+						"provider": "github_app",
+						"repo": map[string]any{
+							"external_id": "repoID",
+						},
+					},
+					"config_ref":   "configRef",
+					"checkout_ref": "checkoutRef",
+				}
+
+				b, err := io.ReadAll(r.Body)
+				assert.NilError(t, err)
+
+				var actualRequestBody any
+				err = json.Unmarshal(b, &actualRequestBody)
+				assert.NilError(t, err)
+
+				assert.DeepEqual(t, actualRequestBody, expectedRequestBody)
+
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+
+				resp, err := json.Marshal(map[string]any{
+					"id":   "123",
+					"name": "testName",
+				})
+				assert.NilError(t, err)
+				_, err = w.Write(resp)
+				assert.NilError(t, err)
+			},
+			want: &trigger.CreateTriggerInfo{
+				Id:   "123",
+				Name: "testName",
 			},
 		},
 		{
@@ -78,30 +137,6 @@ func Test_triggerRestClient_CreateTrigger(t *testing.T) {
 			},
 			wantErr: true,
 		},
-		{
-			name: "Should handle a successful request with CreateTrigger with checkoutRef and configRef",
-			handler: func(w http.ResponseWriter, r *http.Request) {
-				assert.Equal(t, r.Header.Get("circle-token"), "token")
-				assert.Equal(t, r.Header.Get("accept"), "application/json")
-				assert.Equal(t, r.Header.Get("user-agent"), version.UserAgent())
-
-				assert.Equal(t, r.Method, "POST")
-				assert.Equal(t, r.URL.Path, fmt.Sprintf("/api/v2/projects/%s/pipeline-definitions/%s/triggers", projectID, pipelineDefinitionID))
-
-				w.Header().Set("Content-Type", "application/json")
-				w.WriteHeader(http.StatusOK)
-				_, err := w.Write([]byte(`
-				{
-					"id": "123",
-					"name": "test-trigger"
-				}`))
-				assert.NilError(t, err)
-			},
-			want: &trigger.CreateTriggerInfo{
-				Id:   "123",
-				Name: testName,
-			},
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -111,18 +146,7 @@ func Test_triggerRestClient_CreateTrigger(t *testing.T) {
 			p, err := getTriggerRestClient(server)
 			assert.NilError(t, err)
 
-			options := trigger.CreateTriggerOptions{
-				ProjectID:            projectID,
-				PipelineDefinitionID: pipelineDefinitionID,
-				Name:                 testName,
-				Description:          description,
-				RepoID:               repoID,
-				EventPreset:          eventPreset,
-				ConfigRef:            configRef,
-				CheckoutRef:          checkoutRef,
-			}
-
-			got, err := p.CreateTrigger(options)
+			got, err := p.CreateTrigger(tt.options)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("triggerRestClient.CreateTrigger() error = %v, wantErr %v", err, tt.wantErr)
 				return
