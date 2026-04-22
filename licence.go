dce515a268749e3cf15981d9ad653f9630287572
Pete Steyert-Woods <pete.steyertwoods@circleci.com>
2026-04-22 11:32:36 +0100

    Setup tool to apply MIT licence automatically
    - Will run as part of `go generate`.
    - Will be validated implicitly by the `lint` CI job

---
+++ licence.go
@@ -0,0 +1,6 @@
+// Copyright Circle Internet Services, Inc. 2026
+// SPDX-License-Identifier: MIT
+
+//go:generate go tool addlicense -c "Circle Internet Services, Inc." -l mit -s -ignore ".idea/**" -ignore ".circleci/**" -ignore ".claude/**" .
+
+package main
