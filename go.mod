9724f00ed3177ef58ccd48b59e2857c13d002c90
Pete Steyert-Woods <pete.steyertwoods@circleci.com>
2026-05-22 11:54:08 +0100

    Add telemetry client

--- go.mod
+++ go.mod
@@ -16,12 +16,14 @@ require (
 	github.com/go-chi/chi/v5 v5.2.5
 	github.com/go-chi/render v1.0.3
 	github.com/gofrs/flock v0.13.0
+	github.com/google/go-cmp v0.7.0
 	github.com/google/uuid v1.6.0
 	github.com/hashicorp/go-retryablehttp v0.7.8
 	github.com/hexops/gotextdiff v1.0.3
 	github.com/itchyny/gojq v0.12.19
 	github.com/njayp/ophis v1.1.4
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c
+	github.com/segmentio/analytics-go/v3 v3.3.0
 	github.com/spf13/cobra v1.10.2
 	github.com/zalando/go-keyring v0.2.8
 	golang.org/x/oauth2 v0.36.0
@@ -37,6 +39,7 @@ require (
 	github.com/alecthomas/chroma/v2 v2.24.1 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/charmbracelet/colorprofile v0.4.3 // indirect
 	github.com/charmbracelet/ultraviolet v0.0.0-20260428153724-66037269d7be // indirect
 	github.com/charmbracelet/x/ansi v0.11.7 // indirect
@@ -49,7 +52,6 @@ require (
 	github.com/dlclark/regexp2 v1.12.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.1 // indirect
 	github.com/godbus/dbus/v5 v5.2.2 // indirect
-	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/jsonschema-go v0.4.3 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
@@ -63,6 +65,7 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/segmentio/asm v1.2.1 // indirect
+	github.com/segmentio/backo-go v1.0.0 // indirect
 	github.com/segmentio/encoding v0.5.4 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
---
+++ internal/telemetry/telemetry.go
@@ -0,0 +1,153 @@
+// Copyright (c) 2026 Circle Internet Services, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// SPDX-License-Identifier: MIT
+
+package telemetry
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/segmentio/analytics-go/v3"
+)
+
+type Client struct {
+	client analytics.Client
+	user   User
+}
+
+type Config struct {
+	// WriteKey is the Segment write key, and if not provided, will disable telemtry.
+	WriteKey string
+	// Endpoint is the Segment endpoint, and is optional, defaulting to segment io.
+	// This is normally only set for testing.
+	Endpoint string
+	// Specifies the number of events to batch together before sending. If zero the client will use a default.
+	// This is likely only useful for testing.
+	BatchSize int
+
+	// User is the user to associate with events.
+	User User
+}
+
+type User struct {
+	// InstanceID allows manually specifying the client instance ID. Not meant to bs used in production, but
+	// useful for deterministic tests.
+	InstanceID uuid.UUID
+	// UserID is the user ID to associate with events.
+	UserID uuid.UUID
+
+	IsSelfHosted bool
+	OS           string
+	Version      string
+}
+
+func (u User) toContext() *analytics.Context {
+	return &analytics.Context{
+		App: analytics.AppInfo{
+			Name:    "circleci-cli",
+			Version: u.Version,
+		},
+		OS: analytics.OSInfo{
+			Name: u.OS,
+		},
+		Device: analytics.DeviceInfo{
+			Id:           u.InstanceID.String(),
+			Manufacturer: "CircleCI Ltd",
+			Name:         "circleci-cli",
+		},
+	}
+}
+
+// New creates a new segment client
+func New(cfg Config) (*Client, error) {
+	if cfg.WriteKey == "" {
+		return nil, nil
+	}
+
+	client, err := analytics.NewWithConfig(cfg.WriteKey, analytics.Config{
+		Endpoint:  cfg.Endpoint,
+		BatchSize: cfg.BatchSize,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create segment client: %w", err)
+	}
+
+	if cfg.User.InstanceID == uuid.Nil {
+		cfg.User.InstanceID = uuid.New()
+	}
+
+	if cfg.User.UserID == uuid.Nil {
+		cfg.User.UserID = AnonymousID
+	}
+
+	return &Client{
+		client: client,
+		user:   cfg.User,
+	}, nil
+}
+
+func (c *Client) Identify() error {
+	if c == nil {
+		return nil
+	}
+
+	return c.client.Enqueue(analytics.Identify{
+		UserId:       c.user.UserID.String(),
+		Context:      c.user.toContext(),
+		Integrations: analytics.NewIntegrations().Enable("Amplitude"),
+	})
+}
+
+func (c *Client) Close() error {
+	return c.client.Close()
+}
+
+// AnonymousID is hard-coded to a well-known value for unknown users.
+// Callers should provide a real user id where possible.
+var AnonymousID = uuid.MustParse("66f35d3e-40f6-4ade-909b-a6314990de53")
+
+// Track sends an analytics event.
+func (c *Client) Track(eventName string, props map[string]any) error {
+	// Allow a nil client so that it can be treated as non-critical, especially during tests.
+	if c == nil {
+		return nil
+	}
+
+	extras := analytics.NewProperties()
+
+	for key, val := range props {
+		extras.Set(key, val)
+	}
+
+	track := analytics.Track{
+		Event:      eventName,
+		Timestamp:  time.Now(),
+		Properties: extras,
+
+		UserId:       c.user.UserID.String(),
+		Context:      c.user.toContext(),
+		Integrations: analytics.NewIntegrations().Enable("Amplitude"),
+	}
+
+	return c.client.Enqueue(track)
+}
